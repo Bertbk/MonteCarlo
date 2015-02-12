911935b7c58fcb195e67933ea747a6be1232b423
Bertrand Thierry <thierry@ljll.math.upmc.fr>
2015-02-12 14:32:59 +0100

    mistake openmp

--- src/Point.cpp
+++ src/Point.cpp
@@ -137,7 +137,7 @@ void Point::LaunchMC()
 #pragma omp critical
 	{
 	  for(int ifun = 0; ifun < Message::GetNFUN() ; ifun++)
-	    resultsMC[ifun]->insert(MyresultsMC.end(), MyresultsMC.begin(), MyresultsMC.end());
+	    resultsMC[ifun]->insert(MyresultsMC[ifun]->end(), MyresultsMC[ifun]->begin(), MyresultsMC[ifun]->end());
 	}
       }//end omp parallel
       //Updating files
